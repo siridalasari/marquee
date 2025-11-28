c3940e2cc830a97ee98db32220f4860e4069e855
Sirisha Dalasari <sirishadalasari@Sirishas-MacBook-Pro.local>
2025-11-28 17:38:46 +0530

    merge-split animation

--- marquee1.js
+++ marquee1.js
@@ -1,35 +1,44 @@
-const clearScreen = (screen, screen2) => {
-  for (const i in screen) {screen[i] = " ", screen2[i] = " "};
+const clearScreen = (screen, screen2, screen1) => {
+  for (const i in screen) {screen[i] = " ", screen2[i] = " ", screen1[i] = " "};
 };
 
-const displayOnScreen = (index, screen, screen2) => 
+const displayOnScreen = (index, screen, screen1, screen2) => 
   {
     const screens = [screen, screen2];
     const ind = index % 2;
     const ind2 = 1 - ind;
-    console.log(screens[ind].join("") + "\n" + screens[ind2].join("")) 
+
+    console.log(screens[ind].join("") +"\n"+ screen1.join("") +"\n" + screens[ind2].join("")) 
   };
 
-const drawScreen = (screen, screen2, name, currentOffset) => {
-  for (let i = name.length - 1; i >= 0; i -= 2) {
-    screen[((currentOffset - name.length) + i) % screen.length] = name[i % name.length];
-    screen2[((currentOffset - name.length) + i+1) % screen2.length] = name[i + 1 % name.length];
+const drawScreen = (screen,screen1, screen2, name, currentOffset) => {
+  for (let i = name.length - 1 ; i >= 0; i -= 2) {
+    if(currentOffset % 2 === 0){
+      screen1[((currentOffset - name.length) + i) % screen.length] = name[i % name.length];
+      screen1[((currentOffset - name.length) + i + 1) % screen.length] = name[i + 1 % name.length];
+    }
+    else{
+
+      screen[((currentOffset - name.length) + i) % screen.length] = name[i % name.length];
+      screen2[((currentOffset - name.length) + i + 1) % screen2.length] = name[i + 1 % name.length];
+    }
   }
 };
 
 const main = () => {
-  const name = "▫️▫️▫️▫️▫️▫️▫️▫️◼️".toUpperCase();
+  const name = "sirisha".toUpperCase();
   const screen = " ".repeat(20).split("");
+  const screen1 = " ".repeat(20).split("");
   const screen2 = " ".repeat(20).split("");
   let offset = 0;
 
   setInterval(() => {
     const index = ++offset % screen.length;
     console.clear();
-    clearScreen(screen, screen2);
-    drawScreen(screen, screen2, name, index);
-    displayOnScreen(index, screen, screen2);
-  }, 200);
+    clearScreen(screen, screen1, screen2);
+    drawScreen(screen, screen1, screen2, name, index);
+    displayOnScreen(index, screen, screen1, screen2);
+  }, 500);
 };
 
 main();
